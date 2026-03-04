3190856b30d753faa3946037ce9e118abd55e031
Suraj Manjhi <surajmanjhi303@gmail.com>
2026-03-04 22:49:04 +0530

    .

--- MERN_STACK_RESTAURANT_RESERVATION/frontend/src/components/About.jsx
+++ MERN_STACK_RESTAURANT_RESERVATION/frontend/src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
               <p>The only thing we're serious about is food.</p>
             </div>
             <p className="mid">
-             At Skye Dine, we’re more than just a place to eat — we’re where good food meets great vibes. Designed for trendsetters and food lovers aged 18–30, we bring you bold continental and Chinese flavors without burning a hole in your pocket.
+             At Junkies, we’re more than just a place to eat — we’re where good food meets great vibes. Designed for trendsetters and food lovers aged 18–30, we bring you bold continental and Chinese flavors without burning a hole in your pocket.
 
 Relax on our rooftop lounge with a stunning airport view — a rare experience in the area. Whether you’re here for our handcrafted coffees from Classic Coffee Blender, bubble tea, tasty platters or just chilling with friends, Skye Dine is your go-to hub for memorable bites and good times.
 
